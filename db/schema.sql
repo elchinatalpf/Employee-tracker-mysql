7ffbb252f3faec1cb5c732acd6c58cf300410748
Javier <elchinatalpf@github.com>
2023-09-03 10:08:55 -0500

    Added seeds with employees, installed doten

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,26 @@ DROP DATABASE IF EXIST employeemanagement_db;
 CREATE DATABASE employeemanagement_db;
 
 USE employeemanagement_db;
+
+CREATE TABLE deparment (
+  id INT AUTO_INCREMENT Primary key,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT Primary key,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL NOT NULL,
+  deparment_id INT NOT NULL,
+  FOREIGN KEY (deparment_id) REFERENCES deparment(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT Primary key,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT
+  FOREIGN KEY (role_id) REFERENCES employee(id) ON DELETE SET NULL
+  FOREIGN key (manager_id) REFERENCES deparment(id) ON DELETE SET NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,49 @@
+INSERT INTO department (name)
+VALUES  ('Marketing'),
+        ('Sales'),
+        ('Finance'),
+        ('Legal'),
+        ('Human Resources'),
+        ('Accounting');
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ('Manager', 120000, 1),
+        ('Senior Marketer', 80000, 1),
+        ('Junior Marketer', 50000, 1),
+        ('Manager', 110000, 2),
+        ('Senior Sales Rep', 75000, 2),
+        ('Junior Sales Rep', 45000, 2),
+        ('Manager', 130000, 3),
+        ('Senior Developer', 90000, 3),
+        ('Junior Developer', 60000, 3),
+        ('Manager', 140000, 4),
+        ('Senior Lawyer', 90000, 4),
+        ('Junior Lawyer', 60000, 4),
+        ('Manager', 125000, 5),
+        ('Senior HR Specialist', 82000, 5),
+        ('Junior HR Assistant', 52000, 5),
+        ('Manager', 115000, 6),
+        ('Senior Accountant', 78000, 6),
+        ('Junior Accountant', 48000, 6);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('James', 'Smith', 1, 1),  -- Manager for Marketing
+        ('Emily', 'Johnson', 2, 2),  -- Manager for Sales
+        ('Michael', 'Williams', 3, 3),  -- Manager for Finance
+        ('Sarah', 'Brown', 4, 4),  -- Manager for Legal
+        ('William', 'Jones', 5, 5),  -- Manager for Human Resources
+        ('Elizabeth', 'Garcia', 6, 6),  -- Manager for Accounting
+        ('David', 'Miller', 1, NULL),
+        ('Jennifer', 'Davis', 1, NULL),
+        ('Charles', 'Rodriguez', 2, NULL),
+        ('Linda', 'Martinez', 2, NULL),
+        ('Joseph', 'Hernandez', 3, NULL),
+        ('Patricia', 'Lopez', 3, NULL),
+        ('Christopher', 'Gonzalez', 4, NULL),
+        ('Barbara', 'Wilson', 4, NULL),
+        ('Daniel', 'Anderson', 5, NULL),
+        ('Paul', 'Thomas', 5, NULL),
+        ('Margaret', 'Taylor', 6, NULL),
+        ('Nancy', 'Moore', 6, NULL),
+        ('Donald', 'Jackson', 1, NULL),
+        ('Rebecca', 'White', 2, NULL);
